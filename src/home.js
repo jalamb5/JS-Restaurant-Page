cbb18ffa4c632da43743fc7ed1323060a0c7486e
Justin Lamb <justinlamb@me.com>
2023-08-01 10:19:29 -0700

    home generated by JS

--- src/home.js
+++ src/home.js
@@ -8,9 +8,16 @@ function renderHome() {
 
   const title = document.createElement('h1');
   title.textContent = "Old Tom's Country Life Pub";
+  
+  const image = new Image(250);
+  image.src = "assets/IMG_2392.jpeg";
+
+  const tagLine = document.createElement('p');
+  tagLine.textContent = "At Old Tom's Country Life Pub, we have all the great ales, traditional English fare, and the world's best catnip";
 
   content.appendChild(title);
+  content.appendChild(image);
+  content.appendChild(tagLine);
 
   body.appendChild(content);
-  console.log('something');
 }

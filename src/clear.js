a128fbeed6bedc6f48de95879ddaff74cfc7a0ce
Justin Lamb <justinlamb@me.com>
2023-08-03 10:06:58 -0700

    implement clear()

---
+++ src/clear.js
@@ -0,0 +1,6 @@
+export default clear;
+
+function clear() {
+  let content = document.getElementById("content");
+  content.remove();
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import renderHome from './home.js';
 import renderTabBar from './tabBar.js';
+import clear from './clear.js';
 
 renderTabBar();
 renderHome();
 
 let homeButton = document.getElementById('home');
 homeButton.onclick = () => {
+  clear();
   renderHome();
 };
 // event listener for click -> renderMenu
